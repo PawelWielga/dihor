eca10b7c07dd0927d38dc6226adea206ef44bb98
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-16 14:14:23 +0200

    Add build artifacts and config (#3)

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
+import Projects from './components/Projects.jsx'
+import Blog from './components/Blog.jsx'
+import Footer from './components/Footer.jsx'
+import './App.css'
+import { sections } from './config.js'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {sections.hero && <Hero />}
+      {sections.about && <About />}
+      {sections.projects && <Projects />}
+      {sections.blog && <Blog />}
+      {sections.footer && <Footer />}
+    </>
+  )
+}
+
+export default App
