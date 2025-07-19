2da64a846a5520969007fcc1c4c9c10363a1075f
PawelWielga <wielgapawel@gmail.com>
2025-07-19 16:37:28 +0200

    migrate to pawelwielga.dihor.pl

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
     <nav id="navbar" className="glass">
       <div className="container">
         <div className="nav-container">
-          <div className="logo">PW</div>
+          <div className="logo">Paweł Wielga</div>
           <ul className={`nav-links ${open ? 'open' : ''}`}>
             <li>
               <Link to="/#home" onClick={closeMenu}>
