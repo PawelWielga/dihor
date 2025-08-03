42f9b828b920025cf753ee895d72b6ee78565a69
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-08-03 21:08:13 +0200

    Remove redundant blog redirect pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { sections } from '../config/sections';
 
 function Navbar() {
   const [open, setOpen] = useState(false);
