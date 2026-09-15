c7990aa5547a9a8bb0e983c5834601b82ed913f3
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-15 11:16:11 +0200

    build: support configurable base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
-  base: '/',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [
     react(),
     viteImagemin({
